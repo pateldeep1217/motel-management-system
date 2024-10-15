dd4eea38ee024323e30b7967801b774e9a473d7b
Deep Patel <patel_deep1217@outlook.com>
2024-10-15 16:02:48 -0400

    Update authentication: migrated from Clerk to NextAuth

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,28 +1,13 @@
-import { Loader2 } from "lucide-react";
-import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-
 import React from "react";
 import Logo from "@/components/svg/Logo";
 
 import FeatureShowcase from "@/components/FeatureShowcase";
+import SignIn from "@/features/auth/components/SignIn";
 
 function SignInPage() {
   return (
-    <div className=" min-h-screen grid grid-cols-1 lg:grid-cols-2">
-      <div className="h-full lg:flex flex-col items-center justify-center px-4 border-2">
-        <div className="text-center pt-5 flex justify-center ">
-          <Logo />
-        </div>
-        <div className="flex items-center justify-center mt-8">
-          <ClerkLoaded>
-            <SignIn />
-          </ClerkLoaded>
-          <ClerkLoading>
-            <Loader2 className="animate-spin text-muted-foreground" />
-          </ClerkLoading>
-        </div>
-      </div>
-      <FeatureShowcase />
+    <div className="mt-10">
+      <SignIn />
     </div>
   );
 }
---
+++ auth.ts
@@ -0,0 +1,65 @@
+import NextAuth from "next-auth";
+import bcrypt from "bcryptjs";
+
+import Credentials from "next-auth/providers/credentials";
+import { db } from "./db";
+import { users } from "./db/schema";
+import { eq } from "drizzle-orm";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+        name: {},
+      },
+      async authorize(credentials) {
+        const [user] = await db
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.email as string));
+
+        if (!user) {
+          throw new Error("incorrect crenditals");
+        } else {
+          const passwordMatch = await bcrypt.compare(
+            credentials.password as string,
+            user.password as string
+          );
+
+          if (!passwordMatch) {
+            throw new Error("Incorrect crenditals");
+          }
+        }
+
+        return {
+          id: user.id.toString(),
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+});
