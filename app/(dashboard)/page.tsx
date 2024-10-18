d545dbe52fa81eb8f21c6efd5bc47d4c1a1cc71b
Deep Patel <patel_deep1217@outlook.com>
2024-10-18 13:24:12 -0400

    add daily room revenue and User Button to sidebar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,13 @@
 import DashboardOverview from "@/features/auth/components/DashboardOverview";
+import DailyRoomRevenue from "./DailyRoomRevenue";
+import { protectServer } from "@/features/auth/utils";
 
-export default function Home() {
+export default async function Home() {
+  await protectServer();
   return (
     <div className="mx-auto max-w-7xl">
       <DashboardOverview />
+      <DailyRoomRevenue />
     </div>
   );
 }
